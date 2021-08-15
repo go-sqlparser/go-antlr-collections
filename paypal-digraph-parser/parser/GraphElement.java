5d9f57fe9865a140da5148b467431cf10a6b8ceb
Tong Sun <suntong@cpan.org>
2021-08-15 00:35:39 -0400

    - [+] add paypal-digraph-parser from paypal

---
+++ paypal-digraph-parser/parser/GraphElement.java
@@ -0,0 +1,69 @@
+/*
+[BSD 3-Clause License]
+
+Copyright (c) 2017, PayPal Holdings, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package com.paypal.digraph.parser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphElement
+{
+	public GraphElement(String id) {
+		mId = id;
+	}
+
+	public void setAttribute(String key, Object value) {
+		mAttrs.put(key, value);
+	}
+
+	public void setAttributes(Map<String,Object> attrs) {
+		mAttrs.putAll(attrs);
+	}
+
+	public String getId() {
+		return mId;
+	}
+
+	public Object getAttribute(String key) {
+		return mAttrs.get(key);
+	}
+
+	public Map<String,Object> getAttributes() {
+		return mAttrs;
+	}
+
+	public String toString() {
+		return getClass().getSimpleName() + "-" + mId + mAttrs;
+	}
+
+	protected String mId;
+	protected Map<String,Object> mAttrs = new HashMap<String,Object>();
+}
